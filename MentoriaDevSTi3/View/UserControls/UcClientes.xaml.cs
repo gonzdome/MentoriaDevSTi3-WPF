f2be727569cdea6b1ebb6faef06ca63970a3f54e
Gabriel <gabrielgdomeniconi@gmail.com>
2021-06-19 18:30:36 -0300

    Elementos adicionados da tela de clientes

---
+++ MentoriaDevSTi3/View/UserControls/UcClientes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MentoriaDevSTi3.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for UcClientes.xaml
+    /// </summary>
+    public partial class UcClientes : UserControl
+    {
+        public UcClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdicionar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
