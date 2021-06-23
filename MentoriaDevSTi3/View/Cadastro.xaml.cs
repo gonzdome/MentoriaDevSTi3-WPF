ae0169736f0b7758874bea03354b678d7c4fb0ba
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-06-23 13:11:22 -0300

    Adicionado: Tela de Cadastro; Adicionado: Botões Retornar e Cadastrar;

---
+++ MentoriaDevSTi3/View/Cadastro.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MentoriaDevSTi3.View
+{
+    /// <summary>
+    /// Interaction logic for Cadastro.xaml
+    /// </summary>
+    public partial class Cadastro : Window
+    {
+        public Cadastro()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRetornar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
