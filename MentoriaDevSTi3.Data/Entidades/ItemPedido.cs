25e423d9552ac3fa3503432b7630df48956aad77
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-06 18:48:15 -0300

    Entidades criadas;

---
+++ MentoriaDevSTi3.Data/Entidades/ItemPedido.cs
@@ -0,0 +1,15 @@
+﻿namespace MentoriaDevSTi3.Data.Entidades
+{
+    public class ItemPedido
+    {
+        public long Id { get; set; }
+        public long PedidoId { get; set; }
+        public long ProtudoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+
+        public Pedido Pedido { get; set; }
+        public Produto Produto { get; set; }
+
+    }
+}
---
+++ MentoriaDevSTi3.Data/Entidades/Produto.cs
@@ -0,0 +1,9 @@
+﻿namespace MentoriaDevSTi3.Data.Entidades
+{
+    public class Produto
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
