354b178f4d4fa7ac5c9a7a78ce1d16cc59cd1e83
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-06 17:29:42 -0300

    Conexão com Banco de Dados;

---
+++ MentoriaDevSTi3.Data/Context/MentoriaDevSTi3Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace MentoriaDevSTi3.Data.Context
+{
+    public class MentoriaDevSTi3Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;port=3306;user=root;password=mentoriadev;database=mentoria_dev;")
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors()
+                .LogTo(x => Debug.WriteLine(x))
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
+
