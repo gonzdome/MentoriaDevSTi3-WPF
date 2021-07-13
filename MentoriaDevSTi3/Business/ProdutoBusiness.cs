9d294a8b4fb584f093ed3bb6860f7f3d2437d138
Gabriel Gonzalez Domeniconi <gabrielgdomeniconi@gmail.com>
2021-07-13 18:57:28 -0300

    ProdutoBusiness estruturado;

---
+++ MentoriaDevSTi3/Business/ProdutoBusiness.cs
@@ -0,0 +1,62 @@
+﻿using MentoriaDevSTi3.Data.Context;
+using MentoriaDevSTi3.Data.Entidades;
+using MentoriaDevSTi3.ViewModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MentoriaDevSTi3.Business
+{
+    public class ProdutoBusiness
+    {
+        private readonly MentoriaDevSTi3Context _context;
+
+        public ProdutoBusiness()
+        {
+            _context = new MentoriaDevSTi3Context();
+        }
+        public void Adicionar(ProdutoViewModel produtoViewModel)
+        {
+            _context.Produtos.Add(new Produto
+            {
+                Nome = produtoViewModel.Nome,
+                Valor = produtoViewModel.Valor
+            });
+
+            _context.SaveChanges();
+
+        }
+
+        public void Alterar(ProdutoViewModel produtoViewModel)
+        {
+            var produto = _context.Produtos.First(x => x.Id == produtoViewModel.Id);
+
+            produto.Nome = produtoViewModel.Nome;
+            produto.Valor = produtoViewModel.Valor;
+
+            _context.SaveChanges();
+        }
+
+        public void Remover(long id)
+        {
+            var produto = _context.Produtos.First(x => x.Id == id);
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+        }
+
+        public List<ProdutoViewModel> Listar()
+        {
+            return _context.Produtos.AsNoTracking()
+                .Select(s => new ProdutoViewModel
+                {
+                    Id = s.Id,
+                    Nome = s.Nome,
+                    Valor = s.Valor
+                }).ToList();
+        }
+
+    }
+}
--- MentoriaDevSTi3/ViewModel/ProdutoViewModel.cs
+++ MentoriaDevSTi3/ViewModel/ProdutoViewModel.cs
@@ -6,6 +6,7 @@ namespace MentoriaDevSTi3.ViewModel
 {
     public class ProdutoViewModel
     {
+        public long Id { get; set; }
         public string Nome { get; set; }
         public decimal Valor { get; set; }
     }
